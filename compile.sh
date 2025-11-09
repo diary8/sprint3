7fdf865805be029fc6bfd0c0c8d76d7cf1aabc23
diary8 <dairy.fitahiana@gmail.com>
2025-11-10 00:38:02 +0300

    compilation

--- compile.sh
+++ compile.sh
@@ -12,10 +12,10 @@ javac -d out \
 jar cf framwork.jar -C out .
 cp framwork.jar ../test/WEB-INF/lib/
 
-cd ../test
-javac -d WEB-INF/classes -cp "/home/diary/Documents/L2/apache-tomcat-10.1.28/lib/servlet-api.jar:WEB-INF/lib/*" src/com/example/*.java
+# cd ../test
+# javac -d WEB-INF/classes -cp "/home/diary/Documents/L2/apache-tomcat-10.1.28/lib/servlet-api.jar:WEB-INF/lib/*" src/com/example/*.java
 
-java -cp "/home/diary/Documents/L2/apache-tomcat-10.1.28/lib/servlet-api.jar:WEB-INF/classes:WEB-INF/lib/*" com.example.ScanTestMain
-jar cvf MyServletApp.war *
-cp MyServletApp.war /home/diary/Documents/L2/apache-tomcat-10.1.28/webapps
-sudo /home/diary/Documents/L2/apache-tomcat-10.1.28/bin/startup.sh
+# java -cp "/home/diary/Documents/L2/apache-tomcat-10.1.28/lib/servlet-api.jar:WEB-INF/classes:WEB-INF/lib/*" com.example.ScanTestMain
+# jar cvf MyServletApp.war *
+# cp MyServletApp.war /home/diary/Documents/L2/apache-tomcat-10.1.28/webapps
+# sudo /home/diary/Documents/L2/apache-tomcat-10.1.28/bin/startup.sh
